517d40b5e9346ac6cb445cb1d41dfa41c9c76024
jeffcoach <156025490+jeffcoach@users.noreply.github.com>
2025-01-09 12:09:58 -0500

    Fold bot
    Co-authored-by: azhang-bv <azhang-bv@users.noreply.github.com>

--- python_skeleton/skeleton/bot.py
+++ python_skeleton/skeleton/bot.py
@@ -2,11 +2,30 @@
 This file contains the base class that you should implement for your pokerbot.
 '''
 
+from skeleton.actions import FoldAction, CallAction, CheckAction, RaiseAction
+from skeleton.states import GameState, TerminalState, RoundState
+from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
+from skeleton.bot import Bot
+from skeleton.runner import parse_args, run_bot
+
+import random
+
 
 class Bot():
     '''
     The base class for a pokerbot.
     '''
+
+    def __init__(self):
+        '''
+        Initialize the bot with hand ranges.
+        '''
+        self.hand_ranges = {
+            "fantastic": {"AA", "KK", "QQ", "JJ", "TT", "AK"},
+            "great": {"AQ", "AJ", "KQ", "88", "99"},
+            "decent": {"22", "33", "44", "55", "66", "77", "T9s", "JTs"},
+            "speculative": {"A5s", "K7s", "QJs", "T8s"}
+        }
 
     def handle_new_round(self, game_state, round_state, active):
         '''
@@ -20,7 +39,7 @@ class Bot():
         Returns:
         Nothing.
         '''
-        raise NotImplementedError('handle_new_round')
+        pass
 
     def handle_round_over(self, game_state, terminal_state, active):
         '''
@@ -34,7 +53,7 @@ class Bot():
         Returns:
         Nothing.
         '''
-        raise NotImplementedError('handle_round_over')
+        pass
 
     def get_action(self, game_state, round_state, active):
         '''
@@ -49,4 +68,12 @@ class Bot():
         Returns:
         Your action.
         '''
-        raise NotImplementedError('get_action')
+        legal_actions = round_state.legal_actions()
+        if RaiseAction in legal_actions:
+            _, max_raise = round_state.raise_bounds()  # Raise
+            return RaiseAction(max_raise)  # All-in
+        elif CallAction in legal_actions:
+            return CallAction()  # Call
+        elif CheckAction in legal_actions:
+            return CheckAction()  # Check
+        return FoldAction()  # Fold
